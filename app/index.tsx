03d5480845f68da610be177750721a65e1f8c5b2
SankshipthShetty <sankshipthshetty@gmail.com>
2024-05-18 21:29:09 +0530

    Initial Setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StatusBar,Image,StyleSheet} from 'react-native'
+import React from 'react'
+import {Link} from 'expo-router'
+export default function App() {
+  return (
+      <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-bold">ResQ</Text>
+    </View>
+  )
+}
+
